8efca5915ee9dc3c08a9813f06ae284b32599666
xaberpl <rav.waszak@gmail.com>
2022-06-03 23:53:41 +0200

    add add-task function

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const addButton = document.querySelector("button.addButton");
 
+const tasks = [];
+
+const addTask = (e) => {
+  e.preventDefault();
+  const titleTask = input.value;
+  if (titleTask == "") {
+    return;
+  }
+  tasks.push(titleTask);
+  ul.textContent = "";
+  input.value = "";
+  tasks.forEach((item) => {
+    const task = document.createElement("li");
+    task.innerHTML = item + "<button>Delete</button>";
+    ul.appendChild(task);
+  });
+};
+addButton.addEventListener("click", addTask);
