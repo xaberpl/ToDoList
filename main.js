0fb22a7f588fcea4c48bb98b870ddb9427ff00b8
xaberpl <rav.waszak@gmail.com>
2022-06-04 23:28:46 +0200

    add task search

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+const input = document.querySelector("input.asd");
+const taskList = document.querySelector("ul.allTasks");
 const addButton = document.querySelector("button.addButton");
+const searchList = document.querySelector("ul.searchList");
+const inputSearch = document.querySelector("input.searchInput");
+const liElements = document.getElementsByClassName("task");
 
 const tasks = [];
+let tasks2 = [];
 
 const addTask = (e) => {
   e.preventDefault();
@@ -12,12 +16,26 @@ const addTask = (e) => {
     return;
   }
   tasks.push(titleTask);
-  ul.textContent = "";
-  input.value = "";
+  taskList.textContent = "";
   tasks.forEach((item) => {
     const task = document.createElement("li");
+    task.classList.add("task");
     task.innerHTML = item + "<button>Delete</button>";
-    ul.appendChild(task);
+    taskList.appendChild(task);
+    input.value = "";
   });
+  console.log(tasks);
 };
 addButton.addEventListener("click", addTask);
+
+const searchTask = (e) => {
+  let searchText = e.target.value;
+  let tasks3 = [...liElements];
+  // console.log(tasks3);
+  tasks3 = tasks3.filter((li) => li.textContent.includes(searchText));
+  console.log(tasks3);
+  searchList.textContent = "";
+  tasks3.forEach((li) => searchList.appendChild(li));
+};
+
+inputSearch.addEventListener("input", searchTask);
