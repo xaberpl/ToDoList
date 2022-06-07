5d409243686d7351dbd36bc43c7ad1f1fe266c56
xaberpl <rav.waszak@gmail.com>
2022-06-07 18:29:23 +0200

    add remove task

--- main.js
+++ main.js
@@ -5,18 +5,26 @@ const addButton = document.querySelector("button.addButton");
 const searchList = document.querySelector("ul.searchList");
 const inputSearch = document.querySelector("input.searchInput");
 const liElements = document.getElementsByClassName("task");
+const taskNumber = document.querySelector("h1 span");
+const deleteButtons = document.getElementsByClassName("deleteButton");
 
 let tasks = [];
 let tasks2 = [];
 
+let count = 0;
+
 const renderTasks = () => {
   taskList.textContent = "";
-  tasks.forEach((item) => {
+  tasks.forEach((item, index) => {
     const task = document.createElement("li");
     task.classList.add("task");
-    task.innerHTML = item + "<button>Delete</button>";
+    task.innerHTML =
+      item + `<button class="deleteButton" data-key=${index}>Delete</button>`;
     taskList.appendChild(task);
     input.value = "";
+  });
+  Array.from(deleteButtons).forEach((button) => {
+    button.addEventListener("click", removeTask);
   });
 };
 
@@ -27,6 +35,7 @@ const addTask = (e) => {
     return;
   }
   tasks.push(titleTask);
+  count++;
   renderTasks();
   console.log(tasks);
 };
@@ -44,8 +53,13 @@ const searchTask = (e) => {
   let tasks3 = tasks2.filter((li) => li.textContent.includes(searchText));
   console.log(tasks3);
   searchList.textContent = "";
-  //tasks3.forEach((li) => searchList.appendChild(li));
   tasks3.forEach((li) => searchList.appendChild(li.cloneNode(true)));
 };
 
 inputSearch.addEventListener("input", searchTask);
+const removeTask = (e) => {
+  const index = e.target.dataset.key;
+  console.log(index);
+  tasks.splice(index, 1);
+  renderTasks();
+};
